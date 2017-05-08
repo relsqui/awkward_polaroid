6fd2867d483594cd8e5b70662bb5b7b178aae929
Finn Ellis <relsqui@chiliahedron.com>
2017-05-08 09:34:37 -0700

    Initial commit.

---
+++ bot.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+
+import tweepy
+import random
+
+from secrets import *
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+twitter = tweepy.API(auth)
+
+with open("for_consuming") as f:
+    grams = f.read().splitlines()
+
+random.shuffle(grams)
+photo_file = grams.pop()
+comment = random.choice([
+        "Hmm ...",
+        "Oh my.",
+        "Awkward.",
+        "Remember this party?",
+        "What was happening here?",
+        "What was his name ...",
+        "What was her name ...",
+        "What was their name ...",
+        "Oh dear.",
+        "Huh.",
+        "Uh ...",
+        "I totally forgot about this.",
+        "Oh geeze.",
+        "I'm going to pretend I never found this.",
+        "This one's going in my scrapbook.",
+        "Don't tell them we found this photo!",
+        "...",
+        "Goodness."
+    ])
+
+tweet = twitter.update_with_media(photo_file, comment)
+
+with open("for_consuming", "w") as f:
+    f.write("\n".join(grams))
+
+with open("tweet_history", "a") as f:
+    f.write("{0}\t{1}\n".format(tweet.id, to_tweet))
