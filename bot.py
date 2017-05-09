4ee3a674a124c2a9fa2d18750c5ac3cad0eca769
Finn Ellis <relsqui@chiliahedron.com>
2017-05-08 20:07:57 -0700

    Just remove files, don't bother with lists.

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@
 
 import tweepy
 import random
+import os
 
 from secrets import *
 
@@ -9,36 +10,24 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 twitter = tweepy.API(auth)
 
-with open("for_consuming") as f:
-    photos = f.read().splitlines()
-
-random.shuffle(photos)
-photo_file = photos.pop()
+photo_file = os.path.join("polaroids", os.listdir("polaroids")[0])
 comment = random.choice([
         "Hmm ...",
-        "Oh my.",
-        "Awkward.",
         "Remember this party?",
-        "What was happening here?",
-        "What was his name ...",
-        "What was her name ...",
-        "What was their name ...",
         "Oh dear.",
         "Huh.",
         "Uh ...",
         "I totally forgot about this.",
         "Oh geeze.",
-        "I'm going to pretend I never found this.",
         "This one's going in my scrapbook.",
-        "Don't tell them we found this photo!",
         "...",
-        "Goodness."
+        "Oh wow, remember this?",
+        "Whose house even was this?",
+        "I don't remember this at all.",
+        "Er ...",
+        "Those were the days.",
+        "I miss that crew."
     ])
 
 tweet = twitter.update_with_media(photo_file, comment)
-
-with open("for_consuming", "w") as f:
-    f.write("\n".join(photos))
-
-with open("tweet_history", "a") as f:
-    f.write("{0}\t{1}\n".format(tweet.id, photo_file))
+os.remove(photo_file)
