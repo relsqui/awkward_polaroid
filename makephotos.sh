925f6d962481c73e45cd1866f04ddb9f6b7cf17c
Finn Ellis <relsqui@chiliahedron.com>
2017-05-08 19:56:12 -0700

    Add lots of polaroids with new grammar.

--- makephotos.sh
+++ makephotos.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-for count in `seq -w 1 1`; do
-    CF3/cfdg -s 500 polaroids.cfdg polaroids/$count.png
+for count in `seq -w 1 999`; do
+    CF3/cfdg -s 627 polaroids.cfdg polaroids/$count.png
 done
