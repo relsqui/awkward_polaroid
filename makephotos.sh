a2c1418e84ab97fa47e45bcda1571ae9e6533617
Finn Ellis <relsqui@chiliahedron.com>
2017-05-08 21:01:47 -0700

    Add copyright and license information.

--- makephotos.sh
+++ makephotos.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Copyright (c) 2017 Finn Ellis.
+# Free to use and modify under the terms of the MIT license.
+# See included LICENSE file for details.
+
 for count in `seq -w 1 999`; do
     CF3/cfdg -s 627 polaroids.cfdg polaroids/$count.png
 done
